472bb707aeaa8c308e65bac28579a703e1efbd66
Asanovich <asansvetlana@yandex.ru>
2023-01-30 19:42:34 +0300

    Нахождение индекса в массиве

---
+++ Experiment/Lesson3/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {17, 13, 34, 57, 12, 67, 91, 107, 34};
+
+int n = array.Length;
+int find = 34;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+{
+    Console.WriteLine(index);
+    break;
+}
+    index ++;
+}
