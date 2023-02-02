7779d9aaa413e4aa52436e6fe6cadc4e4afdfe58
Asanovich <asansvetlana@yandex.ru>
2023-02-02 21:40:53 +0300

    Нахождение случайного числа из отрезка 10-99 и отражение наибольшей цифры числа

---
+++ Experiment/Seminar2/Zadacha1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+int n = new Random().Next(10, 100); //[10, 99]
+Console.WriteLine(n);
+int n1 = n / 10;
+int n2 = n % 10;
+if (n1>n2)
+    Console.WriteLine(n1);
+else
+    Console.WriteLine(n2);
+
