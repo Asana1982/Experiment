6d0d3d4a56553fb38875b39a16a074616026aa1e
Asanovich <asansvetlana@yandex.ru>
2023-02-02 22:34:59 +0300

    Вывод случайного трехзначного числа и вывод на экран первого и последнего чисел

---
+++ Experiment/Seminar2/Zadacha2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear ();
+int n = new Random().Next(100, 1000); //[100, 999]
+Console.WriteLine(n);
+int n1 = n / 100;
+n1 = n1 * 10;
+int n2 = n % 10;
+int k = n1 + n2;
+Console.WriteLine(k);
