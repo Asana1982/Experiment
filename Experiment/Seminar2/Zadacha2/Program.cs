df57b27a398a44d5370e8a852dba61c307669cda
Asanovich <asansvetlana@yandex.ru>
2023-02-03 21:10:01 +0300

    Второй вариант решения задачи. Первый в 1 коммите

--- Experiment/Seminar2/Zadacha2/Program.cs
+++ Experiment/Seminar2/Zadacha2/Program.cs
@@ -2,7 +2,5 @@
 int n = new Random().Next(100, 1000); //[100, 999]
 Console.WriteLine(n);
 int n1 = n / 100;
-n1 = n1 * 10;
-int n2 = n % 10;
-int k = n1 + n2;
-Console.WriteLine(k);
+int n3 = n % 10;
+Console.WriteLine(n1 * 10 + n3);
