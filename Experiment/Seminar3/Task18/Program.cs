31845c48a5bdfb75a5eaa30f0e92daec9796ad25
Asanovich <asansvetlana@yandex.ru>
2023-02-08 21:00:19 +0300

    Определение координат по расположению в четверти оси

---
+++ Experiment/Seminar3/Task18/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите код четверти от 1 до 4:  ");
+double number_Quarter = Convert.ToDouble(Console.ReadLine());
+if (number_Quarter == 1)
+    Console.WriteLine("x > 0 && y > 0");
+if (number_Quarter == 2)
+    Console.WriteLine("x < 0 && y > 0)");
+if (number_Quarter == 3)
+    Console.WriteLine("x < 0 && y < 0)");
+if (number_Quarter == 4)
+    Console.WriteLine("x > 0 && y < 0)");
+ if (number_Quarter != 1 && number_Quarter != 2 && number_Quarter != 3 && number_Quarter != 4);
+    Console.WriteLine("Введите код четверти от 1 до 4:  ");
+   
