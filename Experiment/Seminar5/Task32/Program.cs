587dc849172f823868f5c4fb3c81284d291bdc6b
Asanovich <asansvetlana@yandex.ru>
2023-02-09 10:42:41 +0300

    Задача 33

---
+++ Experiment/Seminar5/Task32/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(-9, 10); // [-9; 9]
+
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+
+for (int i = 0; i < array.Length; i++)
+    array[i] *= (-1);
+
+Console.WriteLine($"Конечный массив: [{string.Join(", ", array)}]");
