9f98123b2202847f08a0988fc56b2ce275fc61ff
Asanovich <asansvetlana@yandex.ru>
2023-02-09 10:31:25 +0300

    Задача 31

---
+++ Experiment/Seminar5/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+int[] array = new int[12];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(-9, 10); // [-9; 9]
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+int sumNegative = 0, sumPositive = 0;
+foreach(int element in array)
+{
+    if (element > 0)
+        sumPositive += element;
+    else
+        sumNegative += element;
+}
+Console.WriteLine($"Сумма положительных чисел {sumPositive}");
+Console.WriteLine($"Сумма отрицательных чисел {sumNegative}");
