82ac8bea7c9d02d3bfdc698e5504c4d09c34cfb9
Asanovich <asansvetlana@yandex.ru>
2023-02-14 17:02:56 +0300

    Замена символов в тексте на иные

--- Experiment/Seminar3/Tablica/Program.cs
+++ Experiment/Seminar3/Tablica/Program.cs
@@ -1,10 +1,21 @@
 ﻿Console.Clear();
-
-for (int i = 2; i <= 10; i++)
+string text =   "несет самостоятельную ответственность по своим обязательствам, может от своего имени"
+            +   "приобретать и осуществлять имущественные и личные неимущественные права, исполнять"
+            +   "обязанности, быть истцом и ответчиком в суде.  НКФО имеет самостоятельный баланс";
+string text_char = $"{text[0]}";
+string Replace(string text, char oldValue, char newValue)
 {
-    for (int j = 2; j <= 10; j++)
+    string result = String.Empty;
+    
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
     {
-        Console.WriteLine($"{i} * {j} = {i * j}");
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
     }
-    Console.WriteLine();
+    return result;
 }
+string newText = Replace(text, ' ', '|');
+string newText1 = Replace(newText, 'п', 'П');
+string newText2 = Replace(newText1, 'с', 'С');
+System.Console.WriteLine(newText2);
