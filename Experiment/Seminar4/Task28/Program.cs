4d3de4cf9ab1e55c84aae274fc42f26d6441ca14
Asanovich <asansvetlana@yandex.ru>
2023-02-15 22:20:51 +0300

    Принимает на число N и выдает произведение числе от 1 до N (факториал)

---
+++ Experiment/Seminar4/Task28/Program.cs
@@ -0,0 +1,8 @@
+﻿// напишите программу которая принимает на вход число N и выдает произведение числе от 1 до N 
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), result = 1;
+for (int i = 1; i <= n; i++)
+    result *= i;
+System.Console.WriteLine($"Результат: {result}");
