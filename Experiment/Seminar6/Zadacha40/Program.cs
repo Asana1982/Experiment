4e3823a92aed575d067ab7e60dd83fa7165a8dcc
Asanovich <asansvetlana@yandex.ru>
2023-02-23 21:21:06 +0300

    Теорема неравенства треугольника

---
+++ Experiment/Seminar6/Zadacha40/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу, которая принимает на вход три числа и проверяет, 
+// может ли существовать треугольник со сторонами такой длины.
+
+Console.Clear();
+Console.Write("Введите длину 1-ой стороны треугольника: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите длину 2-ой стороны треугольника: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите длину 3-й стороны треугольника: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a + b > c && b + c > a && a + c > b)
+  Console.WriteLine("yes");
+else
+  Console.WriteLine("no");
