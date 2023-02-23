4e0f5cb9c3049a0f05c7ec1dd7718abb46ce1a65
Asanovich <asansvetlana@yandex.ru>
2023-02-23 21:12:45 +0300

    Переворот одномерного массива наоборот

---
+++ Experiment/Seminar6/Zadacha39/Program.cs
@@ -0,0 +1,16 @@
+﻿//Переворот одномерного массива наоборот
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+  array[i] = new Random().Next(1, 11); // [1, 10]
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+for(int i = 0; i < array.Length / 2; i++) // цикл переворота одномерного массива (с конца в начало)
+{
+  int temp = array[i];
+  array[i] = array[array.Length - 1 - i];
+  array[array.Length - 1 - i] = temp;
+}
+Console.WriteLine($"Конечный массив: [{string.Join(", ", array)}]");
